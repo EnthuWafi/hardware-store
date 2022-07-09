4ac9f1ecc145bc5ff5b8d2a333e9dd0db945a7cd
EnthuWafi <wafithird@gmail.com>
2022-07-09 21:52:35 +0800

    Bill Fix

--- Bill.java
+++ Bill.java
@@ -22,7 +22,7 @@ public class Bill {
     Bill(Customer customer){
         fullName = customer.getFullName();
         totalCost = customer.getShoppingCart().calculateCart();
-        totalQuantity = customer.getShoppingCart().getNumberOfItems();
+        totalQuantity = customer.getShoppingCart().getItemList().size();
         cart = customer.getShoppingCart();
 
         dateTime = LocalDateTime.now();
@@ -41,8 +41,8 @@ public class Bill {
         System.out.printf("%nBill Generated on %s", getDate());
         System.out.printf("%n%-5s %-30s %-16s %-15s %-3s   %-10s%n", "No", "Item Name", "Unit Price", "Item Discount", "Oty", "Total");
         System.out.print("----------------------------------------------------------------------------------------\n");
-        for (int i = 0; i < cart.getNumberOfItems(); i++){
-            CartItem item = cart.getCart().get(i);
+        for (int i = 0; i < cart.getItemList().size(); i++){
+            CartItem item = (CartItem) cart.getItemList().get(i);
 
             System.out.printf("%-5s %-30s RM%11.2f %16s %5d   RM%11.2f%n", (i+1)+".", item.getItemName(),
                     item.getCost(), item.discountInPercentage()+"%", item.getQuantity(), item.calculateCost());
