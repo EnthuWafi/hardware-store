354ad803bee2369a6be2ad6114bcf2b857550066
EnthuWafi <wafithird@gmail.com>
2022-07-13 19:51:36 +0800

    Major changes in Main and Bill (and a bit of change in User subclasses)
    - Implemented File IO

--- Bill.java
+++ Bill.java
@@ -1,44 +1,48 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Bill {
     //variables
-    private final String fullName;
-    private final ShoppingCart cart;
-    private final double totalCost;
-    private final int totalQuantity;
+    private String fullName;
+    private String userID;
+    private ShoppingCart cart;
+    private double totalCost;
+    private int totalQuantity;
 
-    private final LocalDateTime dateTime;
+    private LocalDateTime dateTime;
 
-    Bill(){
-        fullName = "";
-        totalCost = 0;
-        totalQuantity = 0;
-        cart = null;
-
-        dateTime = LocalDateTime.now();
-    }
+    private PrintWriter billsOutput;
 
     Bill(Customer customer){
         fullName = customer.getFullName();
+        userID = customer.getUserID();
         totalCost = customer.getShoppingCart().calculateCart();
         totalQuantity = customer.getShoppingCart().getItemList().size();
         cart = customer.getShoppingCart();
 
         dateTime = LocalDateTime.now();
+
+        try {
+            billsOutput = new PrintWriter("bills\\" + userID + "\\" + getDateTime() + ".txt");
+        } catch (FileNotFoundException e) {
+            System.out.println(e.toString());
+        }
     }
 
 
     public String getFullName(){return fullName;}
     public int getTotalQuantity(){return totalQuantity;}
     public double getTotalCost(){return totalCost;}
-    public String getDate(){
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    public String getDateTime(){
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH-mm-ss");
         return dateTime.format(dateTimeFormatter);
     }
 
     public void printBill(){
-        System.out.printf("%nBill Generated on %s", getDate());
+        //console
+        System.out.printf("%nBill Generated on %s", getDateTime());
         System.out.printf("%n%-5s %-30s %-16s %-15s %-3s   %-10s%n", "No", "Item Name", "Unit Price", "Item Discount", "Oty", "Total");
         System.out.print("----------------------------------------------------------------------------------------\n");
         for (int i = 0; i < cart.getItemList().size(); i++){
@@ -50,5 +54,20 @@ public class Bill {
         System.out.print("\n----------------------------------------------------------------------------------------\n");
         System.out.printf("%-73s   RM%11.2f%n", "Order Total", cart.calculateCart());
         System.out.print("----------------------------------------------------------------------------------------\n");
+
+        //file output
+        billsOutput.printf("%nBill Generated on %s", getDateTime());
+        billsOutput.printf("%n%-5s %-30s %-16s %-15s %-3s   %-10s%n", "No", "Item Name", "Unit Price", "Item Discount", "Oty", "Total");
+        billsOutput.print("----------------------------------------------------------------------------------------\n");
+        for (int i = 0; i < cart.getItemList().size(); i++){
+            CartItem item = (CartItem) cart.getItemList().get(i);
+
+            billsOutput.printf("%-5s %-30s RM%11.2f %16s %5d   RM%11.2f%n", (i+1)+".", item.getItemName(),
+                    item.getCost(), item.discountInPercentage()+"%", item.getQuantity(), item.calculateCost());
+        }
+        billsOutput.print("\n----------------------------------------------------------------------------------------\n");
+        billsOutput.printf("%-73s   RM%11.2f%n", "Order Total", cart.calculateCart());
+        billsOutput.print("----------------------------------------------------------------------------------------\n");
+        billsOutput.close();
     }
 }
